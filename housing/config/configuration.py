ec92c622a41408117c1dcb5f2e44ddf7603dd90c
Bittu <bittukumar30dec2002@gmail.com>
2023-07-05 03:44:07 +0530

    updated codes

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -15,10 +15,34 @@ from housing.exception import HousingException
 class configuartion:
    
     def __init__(self,config_file_path:str = CONFIG_FILE_PATH,current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
-        self.config_info = read_yaml_file(file_path=config_file_path)
+        try:
+            self.config_info = read_yaml_file(file_path=config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-        pass
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+            data_ingestion_artifact_dir = os.path.join(artifact_dir,DATA_INGESTION_ARTIFACT_DIR,self.time_stamp)
+
+            data_ingestion_info = self.config_info(DATA_INGESTION_CONFIG_KEY)
+
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+
+            tgz_download_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY])
+
+            raw_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY])
+
+            ingested_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY])
+
+            ingested_train_dir = os.path.join(ingested_data_dir,data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY])
+
+            ingested_test_dir = os.path.join(ingested_data_dir,data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY])
+            
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     # The return type for get_data_ingestion_config will be entity (DataIngestionConfig). So, we have to import configuration.py from housing.entity.config_entity . similarly , for other functions also .
 
