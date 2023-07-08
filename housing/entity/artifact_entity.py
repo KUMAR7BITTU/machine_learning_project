35389d35345840c1750568f00c68e485f4612818
Bittu <bittukumar30dec2002@gmail.com>
2023-07-09 01:16:49 +0530

    updated artifact_entity.py

--- housing/entity/artifact_entity.py
+++ housing/entity/artifact_entity.py
@@ -2,7 +2,7 @@
 from collections import namedtuple
 
 # We are defining output for data-ingestion component . And we are going to give some names the output that we are expecting from  data ingestion components .
-DataIngestionArtifact = namedtuple("DataIngestionArtifact",["","",""])
+DataIngestionArtifact = namedtuple("DataIngestionArtifact",["train_file_path","test_file_path","is_ingested","message"])
 
 # We have not defined DataIngestionArtifact in config-entity.py beacause it is for output component(artifact) that we will get . But in config-entity.py we have defined only input(configuration) for special components .So, as DataIngestionArtifact is output component that'sway we have created seperate file for it .
 
@@ -10,8 +10,10 @@ DataIngestionArtifact = namedtuple("DataIngestionArtifact",["","",""])
 
 # The output for DataIngestionArtifact will be downloaded file, extract file , train file , test file . So, this can be the artifact .
 
+# We need to define output . Right now , training file and test file is required for other component (like data validation) . So, we will try to take two to three properties like training file location and test file location so that it could be utilized for data transformation and data validation steps . One more output is whether it was successful or not along with we can also write some messages .
 
 
+# So, now we will import this DataingestionArtifact in data_ingestion.py in component folder . Refer data_ingestion.py
 
 
 
